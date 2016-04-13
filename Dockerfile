1bdfb0b084c58bc5fc41001768f471ca1fc85c13
Austin <austin.griffith@madwiremedia.com>
2016-04-13 08:24:12 -0600

    make it simple

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,6 @@
+#choose your php/apache version here: https://hub.docker.com/_/php/
 FROM php:7.0.5-apache
-
-#RUN apt-get update
-#RUN apt-get upgrade -y
-#RUN apt-get install -y htop
 
 RUN docker-php-ext-install mysqli
 
-ADD bootstrap.sh /root/bootstrap.sh
-
-CMD /root/bootstrap.sh
+CMD /bin/bash -c "source /etc/apache2/envvars && exec /usr/sbin/apache2 -DFOREGROUND"
--- run.sh
+++ run.sh
@@ -2,8 +2,10 @@
 echo "Removing existing mysql container...";
 docker rm -f mysql
 echo "Launching mysql container...";
-docker run --name="mysql" -e MYSQL_ROOT_PASSWORD=supersecret123  -d mysql:5.7.12
+#choose your mysql tag here: https://hub.docker.com/_/mysql/
+docker run --name="mysql" -e MYSQL_ROOT_PASSWORD=supersecret123 -d mysql:5.7.12
 echo "Removing existing container (if it exists)...";
 docker rm -f lamp
-echo "Launching apache container...;"
+echo "Launching apache container..."
 docker run --name="lamp" --link mysql:mysql  -v ${PWD}/html:/var/www/html -v ${PWD}/mysqldata:/var/lib/mysql -p 8080:80 -d lamp
+docker ps
